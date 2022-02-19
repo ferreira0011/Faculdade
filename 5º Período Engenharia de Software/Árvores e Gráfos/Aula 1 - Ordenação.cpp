39e06c50dcdc1a96d063f58d488cfea6f09fd711
Victor Alves Ferreira <84113257+ferreira0011@users.noreply.github.com>
2022-02-18 21:46:42 -0300

    Add files via upload

--- 5º Período Engenharia de Software/Árvores e Gráfos/Aula 1 - Ordenação.cpp
+++ 5º Período Engenharia de Software/Árvores e Gráfos/Aula 1 - Ordenação.cpp
@@ -13,31 +13,60 @@ Ordem crescente, menor para o maior
 mostrar o vetor final
 
 */
+//Variáveis globais
+int vet[tam], i=0, j=0, troca=0;
+
+void preencherVetor(void);//Preenchendo o vetor
+void imprimirVetor(void);//Imprimir vetor
+void ordenacaoBubbleSort(void);//Ordenando o vetor Bubble sort
+void ordenacaoSelectionSort(void);//Ordenando o vetor Selection sort
 
 int main() {
 	setlocale(LC_ALL, "Portuguese");//habilita a acentuação para o português
 	
-	//rand () % 10
-	int vet[tam], i=0, j=0, troca=0;
-	
 	//Preenchendo o vetor
-	for(i=0; i<tam; i++) {
-		//printf("Digite o %dº valor: ", i +1);
-		//scanf("%d", &vet[i]);
-		vet[i] = rand () % tam;
-		//system("cls"); //Limpar tela
-		
-	}
+	preencherVetor();
 	
 	//Imprimindo o vetor sem ordenação
 	printf("Vetor sem ordenação: ");
+	imprimirVetor();
+	printf("\n\n");
+	
+	//Bubble sort
+	//Ordenando o vetor Bubble sort
+	void ordenacaoBubbleSort();
+	
+	//Imprimindo o vetor com ordenação bubble sort
+	printf("Vetor com ordenação Bubble Sort: ");
+	imprimirVetor();
+	
+	//Selection sort
+	//Ordenando o vetor Selection sort
+	void ordenacaoSelectionSort();
+	
+	
+}
+
+//Preenchendo o vetor
+void preencherVetor(void) {
+		for(i=0; i<tam; i++) {
+		//printf("Digite o %dº valor: ", i +1);
+		//scanf("%d", &vet[i]);
+		//system("cls"); //Limpar tela
+		vet[i] = rand () % tam;	
+	}
+}
+
+//Imprimir vetor
+void imprimirVetor(void) {
 	for(i=0; i<tam; i++) {
 		printf("%d ", vet[i]);
 		
 	}
-	printf("\n\n");
-	
-	//Ordenando o vetor
+}
+//Bubble sort
+//Ordenando o vetor bubble sort
+void ordenacaoBubbleSort(void) {
 	for(i=0; i<tam; i++) {
 		for(j=i+1; j<tam; j++) {
 			if (vet[i]>vet[j]) {
@@ -47,14 +76,9 @@ int main() {
 			}
 		}
 	}
-	
-	//Imprimindo o vetor com ordenação
-	printf("Vetor com ordenação: ");
-	for(i=0; i<tam; i++) {
-		printf("%d ", vet[i]);
-		
-	}
-	
-	
+}
+//Selection sort
+//Ordenando o vetor Selection sort
+void ordenacaoSelectionSort(void) {
 	
 }
