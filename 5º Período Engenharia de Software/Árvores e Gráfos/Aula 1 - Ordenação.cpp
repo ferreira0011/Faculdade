0b36768f58e60ad841b19ddfd8e9dbe95a8648f0
Victor Alves Ferreira <84113257+ferreira0011@users.noreply.github.com>
2022-02-23 15:14:48 -0300

    Add files via upload

--- 5º Período Engenharia de Software/Árvores e Gráfos/Aula 1 - Ordenação.cpp
+++ 5º Período Engenharia de Software/Árvores e Gráfos/Aula 1 - Ordenação.cpp
@@ -1,27 +1,28 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include <locale.h>
-#define tam 10
+#include<locale.h>
+#define tam 7
 
 /*
-
-Ordenação bubble sort
-
+Ordenação bubble sort e Selection sort
 Algoritmo de ordenação
 Vetor 10 posições inteiros aleatórios.
 Ordem crescente, menor para o maior
 mostrar o vetor final
-
 */
-//Variáveis globais
-int vet[tam], i=0, j=0, troca=0;
 
+// tam == tamanho do vetor
+
+//Variáveis globais
+int vet[tam], i=0, j=0, troca=0, menor=0;
+
+//Protótipo das Funções
 void preencherVetor(void);//Preenchendo o vetor
 void imprimirVetor(void);//Imprimir vetor
 void ordenacaoBubbleSort(void);//Ordenando o vetor Bubble sort
 void ordenacaoSelectionSort(void);//Ordenando o vetor Selection sort
 
-int main() {
+main() {
 	setlocale(LC_ALL, "Portuguese");//habilita a acentuação para o português
 	
 	//Preenchendo o vetor
@@ -34,18 +35,27 @@ int main() {
 	
 	//Bubble sort
 	//Ordenando o vetor Bubble sort
-	void ordenacaoBubbleSort();
+	ordenacaoBubbleSort();
 	
 	//Imprimindo o vetor com ordenação bubble sort
 	printf("Vetor com ordenação Bubble Sort: ");
 	imprimirVetor();
+	printf("\n\n");
 	
 	//Selection sort
 	//Ordenando o vetor Selection sort
 	void ordenacaoSelectionSort();
 	
+	//Imprimindo o vetor com ordenação Selection sort
+	printf("Vetor com ordenação Selection Sort: ");
+	imprimirVetor();
+	printf("\n\n");
+	
+	
 	
 }
+
+//Funções
 
 //Preenchendo o vetor
 void preencherVetor(void) {
@@ -56,20 +66,12 @@ void preencherVetor(void) {
 		vet[i] = rand () % tam;	
 	}
 }
-
-//Imprimir vetor
-void imprimirVetor(void) {
-	for(i=0; i<tam; i++) {
-		printf("%d ", vet[i]);
-		
-	}
-}
 //Bubble sort
 //Ordenando o vetor bubble sort
 void ordenacaoBubbleSort(void) {
 	for(i=0; i<tam; i++) {
 		for(j=i+1; j<tam; j++) {
-			if (vet[i]>vet[j]) {
+			if (vet[i] > vet[j]) {
 				troca = vet[i];
 				vet[i] = vet[j];
 				vet[j] = troca;
@@ -80,5 +82,23 @@ void ordenacaoBubbleSort(void) {
 //Selection sort
 //Ordenando o vetor Selection sort
 void ordenacaoSelectionSort(void) {
-	
+	for(i=0; i<tam-1; i++) {
+		menor = i;
+		for(j=i+1; j<tam; j++) {
+			if(vet[j] < vet[menor]) {
+				menor = j;
+			}
+		}
+		if(i != menor) {
+			troca = vet[i];
+			vet[i] = vet[menor];
+			vet[menor] = troca;
+		}
+	}
+}
+//Imprimir vetor
+void imprimirVetor(void) {
+	for(i=0; i<tam; i++) {
+		printf("%d ", vet[i]);
+	}
 }
